2f9af8d0d420c368fe71e7a80f3456521e5923e0
Sackertt <notsackert@gmail.com>
2023-05-25 18:19:05 -0400

    Update index.blade.php
    ahora esta las visats para ver y agregar y las de index

--- proyecto laravel/certamen2.1/resources/views/administradores/index.blade.php
+++ proyecto laravel/certamen2.1/resources/views/administradores/index.blade.php
@@ -1,1 +1,42 @@
-hola
+@extends('templates.master')
+@section('contenidoPrincipal')
+<div class="container p-5">
+        <h1 class="text-center text-white">Vista de Administrador</h1>
+      <div class="row">
+          <div class="col d-flex">
+              <div class="card">
+                 <h1 class="card-header bg-primary text-white">Administrar Usuarios</h1>
+                  <div class="card-body d-flex flex-column">
+                      <p class="card-text flex-fill ">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum ipsam officiis quasi accusamus reprehenderit minus voluptate eum similique nesciunt omnis. Expedita doloremque sequi accusantium eligendi consequatur sit unde ipsum ex.
+                      </p>
+                      <div class="row d-flex align-items-center">
+                          <div class="col text-start " >
+                              <a href="" class="btn btn-secondary">
+                              Administrar Estudiantes y Profesores
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col d-flex">
+              <div class="card">
+                 <h1 class="card-header bg-primary text-white">Modificar Estado Proyectos</h1>
+                  <div class="card-body d-flex flex-column">
+                      <p class="card-text flex-fill">
+                          Lorem ipsum, dolor sit amet consectetur adipisicing Earum ipsam officiis quasi accusamus reprehenderit minus voluptate eum similique nesciunt omnis. Expedita doloremque sequi accusantium eligendi consequatur sit unde ipsum ex.
+                      </p>
+                      <div class="row d-flex align-items-center">
+                          <div class="col" >
+                              <a href="" class=" btn btn-secondary">
+                                  Modificar Estado Propuesta
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+@endsection
